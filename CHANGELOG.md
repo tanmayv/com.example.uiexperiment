39fb33938a5ceedbfb7858149a53b5f2b49735f1
Tanmay Vijayvargiya <12tanmayvijay@gmail.com>
2024-06-29 04:23:31 +0530

    NgFor in UI5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -22,3 +22,30 @@
 - Currently we render Main component first, it checks for post login state and then navigate accordingly.
 - Ideally you would want to have a check before navigation, but UI5 has no concept of blocking the navigation, so while you are checking for post login it will render the home page. Dumb bitch!
 - Only good way to solve this would to setup multiple targets in UI5. https://sapui5.hana.ondemand.com/sdk/#/topic/2c5c84d207d246bc9f733f29df1ff892. Or maybe using nested routing. Basic idea is to have a common component being initialized/render before the actual content. We can then check for post login in that common component. This would allows us to Guard the contents without actually writing post login check in every component. This is all theoretical as I cant bother with navigation anymore. I miss angular. 
+
+# NgFor but in UI5.
+- So all the container objects can be used in two ways. Either manually creating all the childs, setting their parameters/attritubes. or We can use aggregator binding. A control can have aggregator binding which can be found from the API reference>Aggregation Tab.
+- In case of Wizard; aggregator binding name is "steps" and the type is "WizardStepItem". So based on this we have following two ways of using it.
+## First option (hardcoded;)
+```html
+<Wizard>
+<WizardStep title="Hello"\>
+<WizardStep title="World"\>
+<\Wizard>
+```
+
+## First option (hardcoded;)
+```ts
+\\suppose the model is set to
+[
+{ "title": "hello" }, { "title": "world" }
+]
+```
+```html
+<Wizard steps="{\}">
+<!-- here steps is like ngfor and it will iterate on the path be gave in Wizard.steps -->
+<steps>  
+<WizardStep title="{title}" content={description}\>
+<\steps>
+<\Wizard>
+```
--- webapp/controller/PostLogin.controller.ts
+++ webapp/controller/PostLogin.controller.ts
@@ -4,6 +4,7 @@ import { PostLoginResponse } from "../types";
 import Wizard from "sap/m/Wizard";
 import WizardStep from "sap/m/WizardStep";
 import Text from "sap/m/Text";
+import JSONModel from "sap/ui/model/json/JSONModel";
 
 /**
  * @namespace com.example.uiexperiment.controller
@@ -16,6 +17,14 @@ export default class PostLogin extends Controller {
 	public onInit(): void {
         console.log("Post Login page OnInit");
 		this.fetchPostLogin().then(this.renderWizard.bind(this));
+        const data = {
+            items: [
+                { title: "Item 1", description: "Description 1" },
+                { title: "Item 2", description: "Description 2" },
+                { title: "Item 3", description: "Description 3" }
+            ]
+        };
+
 	}
 
 	private async fetchPostLogin() {
@@ -31,17 +40,19 @@ export default class PostLogin extends Controller {
 	}
 
 	private renderWizard(wizardData: PostLoginResponse[]) {
-		const wizard: Wizard = this.byId("id.postLoginWizard") as Wizard;
-		wizard.removeAllSteps();
-        console.log("Post login response", wizardData);
-		for (const stepdata of wizardData) {
-			const wizardStep = new WizardStep(stepdata.key, {
-				title: stepdata.pageContext.title,
-				validated: true, /* This needs to be true to show the next step button */
-				content: [new Text({ text: stepdata.pageContext.description })],
-			});
-			wizard.addStep(wizardStep);
-		}
+        const oModel = new JSONModel(wizardData);
+        this.getView().setModel(oModel);
+		// const wizard: Wizard = this.byId("id.postLoginWizard") as Wizard;
+		// wizard.removeAllSteps();
+        // console.log("Post login response", wizardData);
+		// for (const stepdata of wizardData) {
+		// 	const wizardStep = new WizardStep(stepdata.key, {
+		// 		title: stepdata.pageContext.title,
+		// 		validated: true, /* This needs to be true to show the next step button */
+		// 		content: [new Text({ text: stepdata.pageContext.description })],
+		// 	});
+		// 	wizard.addStep(wizardStep);
+		// }
 
 	}
 }
